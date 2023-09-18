2cc202ed2909a62bd92493b113137ab33cf8c369
keisuke-a <36350477+keisuke-a@users.noreply.github.com>
2023-09-18 16:45:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -44,12 +44,15 @@ if "messages" not in st.session_state:
 # チャットボットとやりとりする関数
 def communicate():
     messages = st.session_state["messages"]
+    bot_message = agent.run(st.session_state["user_input"]) #シンプルに修正
+    messages.append(bot_message)　#シンプルに修正
+
     
-    if len(messages) > 0:
-        user_message = {"role": "user", "content": st.session_state["user_input"]}
-        messages.append(user_message)
-        bot_message = agent.run("1+1") #ここ作業中
-        messages.append(bot_message)
+#    if len(messages) > 0:
+#        user_message = {"role": "user", "content": st.session_state["user_input"]}
+#        messages.append(user_message)
+#        bot_message = agent.run(st.session_state["user_input"])
+#        messages.append(bot_message)
 
     st.session_state["user_input"] = ""  # 入力欄を消去
 
