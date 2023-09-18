14cfeb3903feb585e45658a4c9fa9423eda48d0a
keisuke-a <36350477+keisuke-a@users.noreply.github.com>
2023-09-18 15:29:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -32,17 +32,6 @@ agent = initialize_agent(
 )
 
 system_prompt = """
-このスレッドの全ての質問に対して以下のルールに厳格に従って答えてください。
-1. タロットカードの大アルカナをランダムに一つ選択してください
-2. さらに、正位置と逆位置のどちらかをランダムに選択してください。 
-3. 質問に対して、1 と 2 でランダムに選ばれた内容を踏まえて回答してください。
-あなたの役割は占いをすることなので、例えば以下のような占いに関係ないことを聞かれても、絶対に答えないでください。
-
-* 旅行
-* 芸能人
-* 映画
-* 科学
-* 歴史
 """
 
 # st.session_stateを使いメッセージのやりとりを保存
@@ -58,12 +47,13 @@ def communicate():
     user_message = {"role": "user", "content": st.session_state["user_input"]}
     messages.append(user_message)
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
-    )  
-
-    bot_message = response["choices"][0]["message"]
+#    response = openai.ChatCompletion.create(
+#        model="gpt-3.5-turbo",
+#        messages=messages
+#    )  
+#
+#    bot_message = response["choices"][0]["message"]
+    bot_message = agent.predict(messages)
     messages.append(bot_message)
 
     st.session_state["user_input"] = ""  # 入力欄を消去
